7ba97cd966a41c18ddeff2325aa3c2915b105bea
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-04 14:39:09 +0100

    Added build of Mu\Https\Request

--- src/Mu/Https/Request.php
+++ src/Mu/Https/Request.php
@@ -6,4 +6,22 @@ namespace Mu\Https;
  * @package Mu\Https\Request
  * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
  */
-class Request extends \Mu\Http\Request {}
+class Request extends \Mu\Http\Request {
+	/**
+	 * Builds the request, used by factory
+	 * @return \Mu\Https\Request|null
+	 */
+	static public function build() {
+	    if ('cli' !== PHP_SAPI) {
+	        $request = new self();
+
+	        if (('http' === $request->getScheme)) {
+	            return null;
+	        }
+
+	        return $request;
+	    }
+
+	    return null;
+	}
+}
