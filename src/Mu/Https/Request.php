66435ed415ba9f501d352740803f9257169159d1
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-04 14:37:28 +0100

    Added Mu\Https\Request

---
+++ src/Mu/Https/Request.php
@@ -0,0 +1,9 @@
+<?php
+namespace Mu\Https;
+
+/**
+ * @category Mu\Https
+ * @package Mu\Https\Request
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ */
+class Request extends \Mu\Http\Request {}
