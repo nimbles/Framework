a2731fc04fd1ee2a914286c769b462a77d3acd6a
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-05 22:57:00 +0100

    Added MIT licence Added start of unit tests for Mu\Http\Request

---
+++ tests/Mu/Http/RequestTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Tests\Mu\Http;
+
+class RequestTest extends \Mu\Http\TestCase {
+    public function testConstruct() {
+        $request = new \Mu\Http\Request();
+
+        $this->assertType('Mu\Core\Request', $request);
+    }
+
+    /**
+     * Tests over the getters which should have the same behavior
+     * @dataProvider getterProvider
+     */
+    public function testGetter($getter) {
+        $request = new \Mu\Http\Request(array(
+            $getter => array(
+                'foo' => 'bar',
+                'baz' => 'qux'
+            )
+        ));
+
+        $method = 'get' . ucfirst($getter);
+
+        $this->assertEquals('bar', $request->$method('foo'));
+        $this->assertEquals('qux', $request->$method('baz'));
+
+        $this->assertType('array', $request->$method());
+        $this->assertNull($request->$method('quux'));
+    }
+
+    public function getterProvider() {
+        return array(
+            array('query'),
+            array('post'),
+            array('session'),
+            array('cookie'),
+        );
+    }
+
+}
