e246879d8123cf91a26034c14b08b0d302020190
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-05 22:59:22 +0100

    Added template file header to Mu.php

--- lib/Mu.php
+++ lib/Mu.php
@@ -2,9 +2,26 @@
 require_once 'Mu/Core/Loader.php';
 
 /**
- * @category Mu
- * @package Mu
+ * Mu Framework
+ *
+ * LICENCE
+ *
+ * This shouce file is subject to the MIT licence that is bundled
+ * with the package in the file LICENCE.md.
+ * It is also available at this URL:
+ * http://mu-framework.com/licence/mit
+ *
+ * @category  Mu
+ * @package   Mu
  * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/licence/mit MIT Licence
+ */
+
+/**
+ * @category  Mu
+ * @package   Mu
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/licence/mit MIT Licence
  */
 class Mu {
 	/**
