c8b5bd23fb1f8bd9eb9a80d795aa9a0e2c62c055
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-09 21:29:58 +0100

    Added license to Http(s) classes

--- lib/Mu/Http/Header/Exception/InvalidHeaderName.php
+++ lib/Mu/Http/Header/Exception/InvalidHeaderName.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Mu Framework
+ *
+ * LICENSE
+ *
+ * This shouce file is subject to the MIT license that is bundled
+ * with the package in the file LICENSE.md.
+ * It is also available at this URL:
+ * http://mu-framework.com/license/mit
+ *
+ * @category  Mu\Http
+ * @package   Mu\Http\Header\Exception\InvalidHeaderName
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/license/mit MIT License
+ */
+
 namespace Mu\Http\Header\Exception;
 
 /**
- * @category Mu\Http
- * @package Mu\Http\Header\Exception\InvalidHeaderName
+ * @category  Mu\Http
+ * @package   Mu\Http\Header\Exception\InvalidHeaderName
  * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
  */
 class InvalidHeaderName extends \Mu\Http\Header\Exception {}
