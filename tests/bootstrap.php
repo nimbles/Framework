8d341229d755b3e2c91c88d8139a905b63b7d8e2
Rob Bruce <rob@robert-bruce.co.uk>
2010-07-12 21:47:40 +0100

    Fixed unit tests for linux

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 define('APPLICATION_ENV', 'test');
 
+// surpress warnings if timezone has not been set on the system
+date_default_timezone_set(@date_default_timezone_get());
+
 require_once realpath(dirname(__FILE__) . '/../lib/Mu.php');
 new Mu();
