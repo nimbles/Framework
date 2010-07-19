a3cd3e80b3fcc5a1f2f05d3f7fa9f4072750d485
mightymephisto <rob@robert-bruce.co.uk>
2010-07-20 00:51:00 +0100

    Added some phpdocs into unit tests

--- tests/Mu/Http/AllTests.php
+++ tests/Mu/Http/AllTests.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Mu Framework
+ *
+ * LICENSE
+ *
+ * This shouce file is subject to the MIT license that is bundled
+ * with the package in the file LICENSE.md.
+ * It is also available at this URL:
+ * http://mu-framework.com/license/mit
+ *
+ * @category  Mu
+ * @package   Mu\Http
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/license/mit MIT License
+ */
+
 namespace Tests\Mu\Http;
 
 require_once 'PHPUnit/Framework.php';
@@ -6,9 +22,11 @@ require_once 'HeaderTest.php';
 require_once 'RequestTest.php';
 
 /**
- * All tests file for Mu Framework
- * @author rob
- *
+ * @category  Mu
+ * @package   Mu\Http
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/license/mit MIT License
+ * @group     Mu\Http
  */
 class AllTests {
 	/**
