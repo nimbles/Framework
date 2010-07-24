c8fb181e262ebcfc71df84e561c3b2a806401441
mightymephisto <rob@robert-bruce.co.uk>
2010-07-24 20:57:09 +0100

    Added mechinism for delegates

---
+++ lib/Mu/Core/Delegates/Exception/DelegateCreationNotAllowed.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Mu Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with the package in the file LICENSE.md.
+ * It is also available at this URL:
+ * http://mu-framework.com/license/mit
+ *
+ * @category  Mu\Core
+ * @package   Mu\Core\Mixin\Exception\DelegateCreationNotAllowed
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/license/mit MIT License
+ */
+
+namespace Mu\Core\Delegates\Exception;
+
+/**
+ * @category  Mu\Core
+ * @package   Mu\Core\Delegates\Exception\DelegateCreationNotAllowed
+ * @copyright Copyright (c) 2010 Mu Framework (http://mu-framework.com)
+ * @license   http://mu-framework.com/license/mit MIT License
+ */
+class DelegateCreationNotAllowed extends \Mu\Core\Delegates\Exception {}
