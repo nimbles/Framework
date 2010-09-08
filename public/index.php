0723220663ecfb888e7c50afc652423daa01b75c
mightymephisto <rob@robert-bruce.co.uk>
2010-09-08 23:08:38 +0100

    Cleaned up index.php

--- public/index.php
+++ public/index.php
@@ -4,17 +4,3 @@ date_default_timezone_set(@date_default_timezone_get());
 
 require_once realpath(dirname(__FILE__) . '/../lib/Mu.php');
 new Mu();
-
-class Test extends Mu\Http\Controller {
-    public function testAction() {
-        $this->getResponse()->setHeader('Content-Type', 'text/plain')
-            ->setBody(print_r($this, true));
-    }
-}
-
-$controller = new Test(
-    new Mu\Http\Request(),
-    new Mu\Http\Response()
-);
-
-$controller->dispatch('testAction');
